b871b45ab2b319524059397c6652e8db717806ca
Michellah <hei.michellah@gmail.com>
2022-05-06 09:12:43 +0200

    fix = remove input

--- decolage.js
+++ decolage.js
@@ -1,6 +1,5 @@
 
 let resultat = [];
-let tab=["6","rocket","boosters","rocket","boosters","rocket"];
 function decolage(tab) {
     for (let element of tab){
         resultat=(tab.filter((x)=>{return x==element}));
