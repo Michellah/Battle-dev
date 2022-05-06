31ff0aeb8012a8cf9845430fff386e7d6fc0a19d
Michellah <hei.michellah@gmail.com>
2022-05-06 08:53:49 +0200

    fix : débuge

--- courtePaille.js
+++ courtePaille.js
@@ -1,8 +1,4 @@
-let tab=["Toky 120",
-"Michela 14",
-"Herilaza 35",
-"Jonatana 8",
-"Hyriol 19"]
+
 function paille(tab){
     let newTab=[];
     tab.forEach(element => newTab.push(element.split(" ")));
