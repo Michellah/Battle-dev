baf14a684c173789e3cafe550378934514e733be
Michellah <hei.michellah@gmail.com>
2022-05-06 09:01:16 +0200

    fix : bug solved

--- decolage.js
+++ decolage.js
@@ -3,6 +3,7 @@ let resultat = [];
 let tab=["6","rocket","boosters","rocket","boosters","rocket"];
 function decolage(tab) {
     for (let element of tab){
+        resultat=(tab.filter((x)=>{return x==element}));
         if (resultat.length == 2) {
             return element
         }
