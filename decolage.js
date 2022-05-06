51998776def55164fd40656069cdd637d2f9e49e
Michellah <hei.michellah@gmail.com>
2022-05-06 08:58:01 +0200

    feat : decolage.js

---
+++ decolage.js
@@ -0,0 +1,13 @@
+
+let resultat = [];
+let tab=["6","rocket","boosters","rocket","boosters","rocket"];
+function decolage(tab) {
+    for (let element of tab){
+        if (resultat.length == 2) {
+            return element
+        }
+    }
+    
+}
+console.log(decolage(tab));
+
