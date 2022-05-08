78003fca5138d693e2dbf75e97a50a2a5b4773c0
Michellah <hei.michellah@gmail.com>
2022-05-08 17:43:29 +0200

    feat ferme.js

---
+++ ferme.js
@@ -0,0 +1,14 @@
+function ferme(mot1,mot2){
+    let tab=""
+    for(let i=0;i<mot1.length;i++){
+       tab=mot1[i]+mot2[i]
+    }
+    return tab
+}
+console.log(ferme('jri','adn'));
+
+/**creer une fonction qui fait la recherche dichotomique
+ * zaraina 2
+ * milatsak eo apovony de mijery oe eo aloha sa ariana anle centre le izy
+ * jerena oe kely sa ngeza
+ * return true or false oe miexsite ao anaty tableau ve le izy*/
